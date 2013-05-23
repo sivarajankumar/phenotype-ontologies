cc78d6735193e67837b1b0c41d4c21fca49f4a28
cmungall <cmungall@054ac9a6-7031-bf61-febd-9d19acfb90e4>
2013-05-23 04:16:59 +0000

    fix

--- trunk/src/tools/flatten-taxon-equivalents.pl
+++ trunk/src/tools/flatten-taxon-equivalents.pl
@@ -1,17 +1,49 @@
 #!/usr/bin/perl
 
-while (<>) {
-
-  #intersection_of: UBERON:0001707 ! nasal cavity
-  #intersection_of: part_of NCBITaxon:10088
-
-    if (/intersection_of: (UBERON:\d+)/) {
-        print "equivalent_to: $1\n";
+my @hdr = ();
+while ($ARGV[0] && $ARGV[0] =~ /^\-/) {
+    my $opt = shift @ARGV;
+    if ($opt eq '-h') {
+        print usage();
     }
-    elsif (/intersection_of:/) {
+    elsif ($opt eq '-i' || '--ont') {
+        @hdr = ("ontology: ".shift(@ARGV));
     }
     else {
-        print;
+        die "unknown option: $opt";
     }
+}
+
+print "$_\n" foreach @hdr;
+print "\n";
+
+while (my $f = shift @ARGV) {
+    
+    #intersection_of: UBERON:0001707 ! nasal cavity
+    #intersection_of: part_of NCBITaxon:10088
+    my $h = 1;
+
+    open(F,$f) || die "no file $f";
+    print STDERR "READING: $f\n";
+    while(<F>) {
+
+        if (/^\[/) {
+            $h = 0;
+        }
+        next if $h;
+
+        if (/intersection_of: (UBERON:\d+)/) {
+            print "equivalent_to: $1\n";
+        }
+        elsif (/intersection_of: (CL:\d+)/) {
+            print "equivalent_to: $1\n";
+        }
+        elsif (/intersection_of:/) {
+        }
+        else {
+            print;
+        }
+    }
+    close(F);
     
 }
