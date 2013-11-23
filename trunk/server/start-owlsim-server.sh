7788b21780a5f723f335c6dc26034992e716b6fa
cmungall <cmungall@054ac9a6-7031-bf61-febd-9d19acfb90e4>
2013-11-23 22:41:17 +0000

    mem

--- trunk/server/start-owlsim-server.sh
+++ trunk/server/start-owlsim-server.sh
@@ -2,5 +2,5 @@
 DATA=../data
 ONT=../src/ontology
 MM=$DATA/Mus_musculus
-owltools --catalog-xml $ONT/catalog-v001.xml all.owl --use-fsim  --sim-load-lcs-cache owlsim.cache --sim-load-ic-cache ic-cache.owl --start-sim-server -p 9031
+OWLTOOLS_MEMORY=12G owltools --catalog-xml $ONT/catalog-v001.xml all.owl --use-fsim  --sim-load-lcs-cache owlsim.cache --sim-load-ic-cache ic-cache.owl --start-sim-server -p 9031
 ####owltools --catalog-xml $ONT/catalog-v001.xml $ONT/mammal.owl --merge-imports-closure --load-instances $MM/Mm-gene-to-phenotype-BF.txt --load-labels $MM/Mm-gene-labels.txt  --fsim-compare-atts -p owlsim-cache.properties -o owlsim-cache.full --start-sim-server -p 9031
