4e3726b68af6a7d94ed91aa5fc4343d2c268338e
cmungall <cmungall@054ac9a6-7031-bf61-febd-9d19acfb90e4>
2014-06-05 23:25:43 +0000

    do not hardcode memory

--- trunk/server/start-owlsim-server.sh
+++ trunk/server/start-owlsim-server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ONT=../src/ontology
-OWLTOOLS_MEMORY=14G owltools --catalog-xml $ONT/catalog-v001.xml all.owl --use-fsim  --sim-load-lcs-cache owlsim.cache --sim-load-ic-cache ic-cache.owl --start-sim-server -p 9031
+owltools --catalog-xml $ONT/catalog-v001.xml all.owl --use-fsim  --sim-load-lcs-cache owlsim.cache --sim-load-ic-cache ic-cache.owl --start-sim-server -p 9031
 #####OWLTOOLS_MEMORY=12G owltools --catalog-xml $ONT/catalog-v001.xml all.owl --use-fsim --sim-load-ic-cache ic-cache.owl --sim-load-lcs-cache owlsim.cache  --start-sim-server -p 9031
 ####owltools --catalog-xml $ONT/catalog-v001.xml $ONT/mammal.owl --merge-imports-closure --load-instances $MM/Mm-gene-to-phenotype-BF.txt --load-labels $MM/Mm-gene-labels.txt  --fsim-compare-atts -p owlsim-cache.properties -o owlsim-cache.full --start-sim-server -p 9031
